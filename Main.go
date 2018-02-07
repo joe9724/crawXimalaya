b2179fe18794c4680adbe07896536ac7a267388d
joe9724 <64060764@qq.com>
2018-02-07 22:22:46 +0800

    d

--- Main.go
+++ Main.go
@@ -5,8 +5,50 @@ import (
 	"log"
 	"github.com/PuerkitoBio/goquery"
 	_"go/doc"
+	"github.com/jinzhu/gorm"
+	"strings"
 )
 
+type Book struct {
+
+	Id int64 `json:"id" gorm:"AUTO_INCREMENT"`
+
+	// author avatar
+	AuthorAvatar string `json:"authorAvatar"`
+
+	// author name
+	AuthorName string `json:"authorName"`
+
+	// clips number
+	ClipsNumber int64 `json:"clipsNumber"`
+
+	// icon
+	Icon string `json:"icon"`
+
+	// name
+	Name string `json:"name"`
+
+	// play count
+	PlayCount int64 `json:"playCount"`
+
+	// show icon
+	ShowIcon bool `json:"showIcon"`
+
+	// status
+	Status int64 `json:"status"`
+
+	// sub category Id
+	SubCategoryID int64 `json:"subCategoryId"`
+
+	// sub title
+	SubTitle string `json:"subTitle"`
+
+	// summary
+	Summary string `json:"summary"`
+
+	// time
+	Time int64 `json:"time"`
+}
 
 func main() {
 	fmt.Println("ok")
@@ -42,11 +84,32 @@ func ExampleScrape() {
 		log.Fatal(err1)
 	}
 
+	db,err := OpenConnection()
+	if err!=nil{
+		fmt.Println(err.Error())
+	}
+	fmt.Println(db.Value)
+
 	d.Find(".name").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
 		fmt.Println("s is",s.Text())
 		//band := s.Find("h4").Text()
 		//title := s.Find("i").Text()
 		//fmt.Printf("Review %d: %s - %s\n", i, band, title)
+		var book Book
+		book.Summary = strings.TrimSpace(s.Text())
+		book.Name = strings.TrimSpace(s.Text())
+		book.AuthorName = "佚名"
+		book.Status = 0
+		book.Icon = "http://tingting-resource.bitekun.xin/resource/image/icon/hlm.jpg"
+		fmt.Println("book.Name is",book.Name)
+		//book.User_id = *(Params.MemberID)
+		//db.Table("books").Create(&book)
 	})
+
+}
+
+func OpenConnection() (db *gorm.DB, err error) {
+	db, err = gorm.Open("mysql", "root:root811123@tcp(106.14.2.153:3306)/tingting?charset=utf8&parseTime=True&loc=Local")
+	return db,err
 }
